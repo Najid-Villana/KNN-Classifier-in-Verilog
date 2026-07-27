78c6fb9a94bf7074d03e5b1037bd15c34e71cd8d
Muhammad Najid <136325575+Najid-Villana@users.noreply.github.com>
2026-07-27 10:56:34 +0500

    Project uploaded

---
+++ KNN_Assignment.srcs/sources_1/new/FSM_Controller.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/07/2026 03:35:21 PM
+// Design Name: 
+// Module Name: FSM_Controller
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FSM_Controller(
+
+    );
+endmodule
---
+++ KNN_Assignment.srcs/sources_1/new/top-k_tracker.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/07/2026 03:34:54 PM
+// Design Name: 
+// Module Name: top-k_tracker
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top-k_tracker(
+
+    );
+endmodule
